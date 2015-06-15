3c44f04245be3b1d18e093f64378fdc67fbd269e
reena29 <reena29@users.noreply.github.com>
2015-06-15 10:33:19 -0400

    Started implementing interactive elements

---
+++ js/menus.js
@@ -0,0 +1,34 @@
+var $select = $("<select></select>");
+$("#d1").append($select);
+
+//Cycle over menu links
+$("li a").each(function(){
+  var $anchor = $(this);
+  //Create an option
+  var $option = $("<option></option>");
+
+  //Deal with selected options depending on current page
+  if($anchor.parent().hasClass("selected")) {
+    $option.prop("selected", true);
+  }
+  //option's value is the href
+  $option.val($anchor.attr("href"));
+  //option's text is the text of link
+  $option.text($anchor.text());
+  //append option to select
+  $select.append($option);
+});
+
+//Bind change listener to the select
+$select.change(function(){
+  //Go to select's location
+  window.location = $select.val();
+});
+
+
+
+
+
+
+
+
