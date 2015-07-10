0f616b4c98e2582ef3f4348dd9055cde10203a77
reena29 <reena29@users.noreply.github.com>
2015-07-10 13:28:31 -0400

    Added more interactive elements using AJAX, Bootstrap and jQuery.

---
+++ js/login.js
@@ -0,0 +1,82 @@
+
+var $password = $("#password");
+var $confirmPassword = $("#confirm_password");
+
+//Hide hints
+$("form span").hide();
+
+function isPasswordValid() {
+  return $password.val().length > 7;
+}
+
+function arePasswordsMatching() {
+  return $password.val() === $confirmPassword.val();
+}
+
+function canSubmit() {
+  return isPasswordValid() && arePasswordsMatching();
+}
+
+function passwordEvent(){
+    //Find out if password is valid  
+    if(isPasswordValid()) {
+      //Hide hint if valid
+      $password.next().hide();
+    } else {
+      //else show hint
+      $password.next().show();
+    }
+}
+
+function confirmPasswordEvent() {
+  //Find out if password and confirmation match
+  if(arePasswordsMatching()) {
+    //Hide hint if match
+    $confirmPassword.next().hide();
+  } else {
+    //else show hint 
+    $confirmPassword.next().show();
+  }
+}
+
+function enableSubmitEvent() {
+  $("#submit").prop("disabled", !canSubmit());
+  
+} 
+
+//When event happens on password input
+$password.focus(passwordEvent).keyup(passwordEvent).keyup(enableSubmitEvent);
+
+//When event happens on confirmation input
+$confirmPassword.focus(confirmPasswordEvent).keyup(confirmPasswordEvent).keyup(enableSubmitEvent);
+
+enableSubmitEvent();
+$("#hidelogin").click(function(){
+$("#loginModal").modal('hide');
+});
+
+
+/*var xhrlogin=new XMLHttpRequest();
+xhrlogin.onreadystatechange = function () {
+  if(xhrlogin.readyState === 4 && xhrlogin.status === 200) {
+        var msg = JSON.parse(xhrlogin.responseText);
+        console.log(msg[0].message);
+$('#wlcm').html(msg[0].message);
+      
+  }
+
+};
+             xhrlogin.open('GET', '../data/welcome.json');
+
+
+$('#submitl').click(function(event){
+event.preventDefault();
+$("#iModal").modal('hide');
+xhrlogin.send();
+});*/
+
+
+
+
+
+
