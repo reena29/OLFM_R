4c65ed7c5ca00278f18caf5384137355e442cf2e
reena29 <reena29@users.noreply.github.com>
2015-07-30 20:25:15 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,4 +1,4 @@
 <?php session_start(); /* Starts the session */
 session_destroy(); /* Destroy started session */
-header("location:home1.php");  /* Redirect to login page */
+header("location:home.php");  /* Redirect to login page */
 ?>
